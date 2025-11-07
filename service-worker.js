96ea2f8ad8f1aa87fc133b3c98910a82ebe31e0a
ZbyszekToKrol <siataturniej@gmail.com>
2025-11-07 21:07:25 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = "PWA-cache";
 const FILES_TO_CACHE = [
   "id.html",
   "assets/main.css",
-  "assets/id.js"
-  "assets/id.css"
+  "assets/id.js",
+  "assets/id.css",
   "assets/manifest.js"
   // add any other pages you want available offline
 ];
